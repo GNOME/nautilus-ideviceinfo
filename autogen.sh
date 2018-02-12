6a9dd28f52798b7a8d5f2031bb8ee261023ba42e
Bastien Nocera <hadess@hadess.net>
2018-02-13 00:23:16 +0100

    build: Fix builddir != srcdir

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+olddir=`pwd`
+
+cd "$srcdir"
+
 aclocal -I m4
 libtoolize
 autoheader
 automake --add-missing
 autoconf
 
+cd "$olddir"
+
 if [ -z "$NOCONFIGURE" ]; then
-    ./configure "$@"
+    "$srcdir"/configure "$@"
 fi
